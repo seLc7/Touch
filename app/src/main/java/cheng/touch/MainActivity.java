ed1be09464d7a80072da9dbbf773729a6b5405f8
Aaron Cheng <372876862@qq.com>
2015-05-15 13:56:02 +0800

    更改颜色获取方式为根据时间 根据时间来获取颜色。现为5分钟，没秒分配一个颜色，共300个点。

--- app/src/main/java/cheng/touch/MainActivity.java
+++ app/src/main/java/cheng/touch/MainActivity.java
@@ -19,7 +19,9 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class MainActivity extends Activity implements Runnable {
@@ -36,18 +38,32 @@ public class MainActivity extends Activity implements Runnable {
     private static final int DONE = 1;
 
     private ArrayList pointRGBList = new ArrayList();
-    private ArrayList lineRGBList = new ArrayList();
+    // private ArrayList lineRGBList = new ArrayList();
     private int count = 0; // 自增量，用于获取colorlist里的颜色
-    private static final int step = 30; // 去颜色的渐变点个数
+    private static final int step = 5 * 60; // 颜色的渐变点个数；5min,每秒分配一个颜色
     private static final int[] beginPoint = {255, 0, 0};  // 起点颜色
-    private static final int[] endPoint = {0, 255, 0}; // 终点颜色
-    private static final int[] beginLine = {0, 0, 255};
+    private static final int[] endPoint = {0, 0, 255}; // 终点颜色
+    /*private static final int[] beginLine = {0, 0, 255};
     private static final int[] endLine = {200, 0, 0};
+    */
+    private long beginTime, endTime, currentTime;
+    Date date; // 获取系统时间
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SimpleDateFormat minuteFormat = new SimpleDateFormat("mm");
+        SimpleDateFormat secondFormat = new SimpleDateFormat("ss");
+
+        date = new Date(System.currentTimeMillis());
+        beginTime = date.getTime();
+        endTime = beginTime + step * 1000;
+
+        String minuteStr = minuteFormat.format(date);
+        String secondStr = secondFormat.format(date);
+        Log.i("time", minuteStr + " " + secondStr + " " + beginTime);
 
         bitmap = BitmapFactory.decodeFile("/sdcard/0.png").copy(Bitmap.Config.ARGB_8888, true);
         canvas = new Canvas(bitmap);
@@ -61,6 +77,8 @@ public class MainActivity extends Activity implements Runnable {
                 switch (action) {
                     case (MotionEvent.ACTION_DOWN):
                         display("ACTION_DOWN", event);
+                        getCount();
+                        Log.i("time", currentTime + "");
                         touchDown(event);
                         break;
                     case (MotionEvent.ACTION_UP):
@@ -88,7 +106,7 @@ public class MainActivity extends Activity implements Runnable {
         };
 
         pointRGBList = shadeColor(beginPoint, endPoint, step);
-        lineRGBList = shadeColor(beginLine, endLine, step);
+        // lineRGBList = shadeColor(beginLine, endLine, step);
     }
 
     private void display(String eventType, MotionEvent event) {
@@ -157,6 +175,16 @@ public class MainActivity extends Activity implements Runnable {
         }
     }
 
+    /*获取颜色点*/
+    private void getCount() {
+        date = new Date(System.currentTimeMillis());
+        currentTime = date.getTime();
+        if (currentTime > endTime) {
+            beginTime=endTime;
+        }
+        count=(int)((currentTime-beginTime)/1000);
+    }
+
     /*生成两个颜色之间step数量个渐变色*/
     private ArrayList shadeColor(int[] beginColor, int[] endColor, int step) {
         ArrayList<int[]> colorList = new ArrayList<>();
@@ -178,7 +206,7 @@ public class MainActivity extends Activity implements Runnable {
     @Override
     public void run() {
 
-        if (newX == oldX && newY == oldY && count < step) {
+        if (newX == oldX && newY == oldY) {
             int[] f = (int[]) pointRGBList.get(count);
             count++;
             Log.i("color", f[0] + " " + f[1] + " " + f[2]);
@@ -187,13 +215,19 @@ public class MainActivity extends Activity implements Runnable {
             paint.setARGB(255, f[0], f[1], f[2]); // 设置颜色
             // paint.setColor(Color.RED);// 设置红色
             canvas.drawCircle(newX, newY, 20, paint); // 绘制点
-        } else if (newX == oldX && newY == oldY && count >= step) {
+        } /*else if (newX == oldX && newY == oldY && count >= step) {
             count = 0;
             int[] f = (int[]) pointRGBList.get(count);
             paint.setStyle(Paint.Style.FILL);
             paint.setARGB(255, f[0], f[1], f[2]); // 设置颜色
             canvas.drawCircle(newX, newY, 20, paint); // 绘制点
-        } else if (newX != oldX && newY != oldY && count < step) {
+        }*/ else {
+            paint.setColor(Color.GREEN);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(newX, newY, 15, paint); // 绘制点
+        }
+
+        /* else if (newX != oldX && newY != oldY && count < step) {
             // paint.setColor(Color.YELLOW);// 设置黄色
             int[] f = (int[]) lineRGBList.get(count);
             paint.setARGB(255,f[0], f[1], f[2]);
@@ -205,7 +239,7 @@ public class MainActivity extends Activity implements Runnable {
             paint.setARGB(255,f[0], f[1], f[2]);
             paint.setStyle(Paint.Style.FILL);
             canvas.drawCircle(newX, newY, 15, paint);
-        }
+        }*/
 
         canvas.save(Canvas.ALL_SAVE_FLAG);
         canvas.restore();
